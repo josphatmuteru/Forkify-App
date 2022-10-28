7a703e39859b2a0def482d3f22a5e0e65b17290d
Josphat Muteru <58342288+josphatmuteru@users.noreply.github.com>
2022-10-28 12:11:27 +0300

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -127,6 +127,10 @@ const controlAddRecipe = async function (newRecipe) {
 
   //Upload new Recipe
 };
+
+const newFeature = function () {
+  console.log('Welcome to the application!');
+};
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -135,6 +139,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 
 init();
--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -35,7 +35,7 @@ class RecipeView extends View {
     return `<figure class="recipe__fig">
     <img src="${this._data.image}" alt="Tomato" class="recipe__img" />
     <h1 class="recipe__title">
-      <span>Pasta${this._data.title}</span>
+      <span>${this._data.title}</span>
     </h1>
     </figure>
     
